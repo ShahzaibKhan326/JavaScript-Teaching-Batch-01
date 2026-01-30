2fbfe8511289f8ebad7f01bf7bdafb4ff2f5eb80
shahzaib khan <kshahzaib07@gmail.com>
2026-01-30 11:15:21 -0800

    Added Class_03

---
+++ Class_03/app.js
@@ -0,0 +1,80 @@
+
+// ------------------>
+
+// if statement condition kay lihai estaml hota hai-->
+
+let a = 3;
+let b = 4;
+
+if(a>b) // true or false  3 > 4 --> false
+{
+    console.log("a is less than be")
+}
+else
+{
+  console.log("a is greater than b")
+}
+
+// syntex --->
+
+// if(condition) ---> true
+// {
+//       true == > condition
+// }
+// else --> false
+// {
+        //  else == > condition
+// }
+
+
+    // 5  /   10
+// if(num > num01)  ==> //false
+    //    10    90
+// if(num > num01)  ==> // false
+  //   10      10
+// if(num == num01)  ==> // true
+    //    10  =  "10"
+// if(num == num01)  ==> // true
+ //    4  =  "4"
+// if(num === num01)  ==> // false
+
+// let userPassword = 10 ;
+// let passwordStore = "10";
+// if(userPassword === passwordStore)
+// {
+//     console.log(true)
+//     console.log(userPassword+passwordStore)
+
+// }
+// else
+// {
+//     console.log(false)
+//     console.log("password is incorrect")
+
+// }
+
+// == / ==> value check kar ga == > "10" or 10 brabar hai
+// === / ==> value check kar ga or type checkare ga == > "10" or 10 brabar  nai hai data type change hai
+
+// if(2 > 10) ==> false
+
+// a = "hello"
+// b = "Hello"
+// if(a == b) ==> false
+
+a = "0";
+b = 0;
+// if(a === b) ==>  false
+// string === number ==> false
+
+//  "10"             10
+// stringNumber == number ==> true
+//  "10"             10
+// stringNumber === number ==> false
+
+a === b 
+// datatype change =>
+
+
+
+
