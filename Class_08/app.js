35eae42a1eccd7bad14502f5bf98b37292b544cc
shahzaib <kshahzaib07@gmail.com>
2026-06-29 12:56:14 -0700

    added class 8

---
+++ Class_08/app.js
@@ -0,0 +1,178 @@
+//  const / let  proptery = value
+
+// property =>
+
+// phone number 
+
+// value =>
+
+// number  => Number
+// string  =>  where thing in the " ";
+// boolean => true / false
+// undifined => value is completey missing or unassigned
+// null     => a value is intentionally set to be empty
+
+// unassigned => porperty is   availbale but value is not .
+
+// let phoneNumber;
+// let PhoneNumber = null ; abi value nai daysakta magar bad may assign hogi.
+
+// console.log(phoneNumber)
+
+// let userName = null;
+
+// function greet(name)
+// {
+//     console.log(name)
+// }
+
+// userName = "shahzaib"
+
+// greet(userName)
+
+// ------>
+
+// ---Student management system----->
+
+// let userName = "shahzaib"; => string => done
+// let rollNumber = 4252627; => Number => done
+// let isFeesPaid = true / false
+// let isOnline = true / false
+// let hasIdcard = true;
+// let haveLaptop = false;  => boolean => done
+
+// let name ;
+// console.log(name) => undifined => done
+
+// let name;
+// console.log(name)
+
+// let obj =
+// {
+//     name:"shahzaib",
+//     age: 20 
+//     // city is missing => undifined
+// }
+
+// console.log(obj.city)
+
+// let obj =
+// {
+//     name:"shahzaib",
+//     age: 20 ,
+//     city : null,
+// }
+
+// console.log(obj.city) => undefined
+
+// missing value ;
+
+
+// ----Difference between null and undefined--->
+
+// undefined =>
+// value is completey missing or unassinged
+// automotically assigned by the javascript engine
+
+// null =>
+// Value is intentionally set to be empty by the developer
+// assigned by developer
+
+// --Example------->
+
+// let stationary =  "pen" ; 
+
+// let evenNumber = 2;
+
+// ------->
+
+// let isFeesPaid = true;
+
+// if(isFeesPaid)  // true
+// {
+//     console.log("access vailable") // block tab chaley ga jab value true hogi
+// }
+// else  // false
+// {
+//     console.log("access not available") // block runs when value is false
+// }
+
+// let userIsPassed = null;
+
+// userIsPassed = true;
+
+// ----date sheet----->
+
+// let english = 60;
+// let urdu = 60;
+// let math = 60;
+
+// let obtainMark = null;
+// let totalMark = 300;
+
+// function totalObtainMark(sub1,sub2,sub3) // perameter
+// {
+//    return sub1+sub2+sub3;
+// }
+
+// obtainMark = totalObtainMark(english,urdu ,math)
+
+// let percentage =( obtainMark / totalMark * 100).toFixed(2);
+// console.log(percentage)
+
+// if(percentage >= 90 )
+// {
+//     console.log("A")
+// }
+// else if (percentage >= 80) // 80 == 80 => true => B
+// {
+//     console.log("B")
+// }
+// else if(percentage >= 50)
+// {
+//   console.log("C")
+// }
+
+
+// ---8 Data types------>
+// string =>  done
+// number => done
+// Boolean =>  done
+// null => done
+// undifined =>  done
+// BigInt => Used for intergers(number) too large to be safely represented by the standart Number => done
+// Symbols =>
+// object =>
+// Array => 
+
+    // BigInt => Used for intergers too large to be safely represented by the standart Number
+
+// let Number = 1223345545434534,
+// let BigInt = 233942349284298424892489284242n; used for big number long number like = > 332342342424244223423
+
+// ----Naming Variable------->
+
+// A-Z a-z 0-9 _  $
+
+// let  name   = "shahzaib";
+//     propery =  value
+
+// NAME , name ,  $name , _name , name1 , Name 
+// 1Name = > staring with number => wrong
+// user name = > with space => wrong 
+
+// name or Name = dono alag varaibale hai 
+
+// let name = "a"
+// let Name = "b"
+
+// console.log(Name) // case sentive
+
+// No reserved words =>
+ 
+// let if = "wrong"
+
+// if => reserved keyword =>
+// Reserved keyword => you cannot use word that js reserve for its own syntex.
+
+
