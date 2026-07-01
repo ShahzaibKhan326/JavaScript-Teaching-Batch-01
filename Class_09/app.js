cb435d10bddb6bc03de32a46d736ee8728652be1
shahzaib <kshahzaib07@gmail.com>
2026-07-01 13:06:52 -0700

    added class 09

---
+++ Class_09/app.js
@@ -0,0 +1,207 @@
+//  Data Types
+// String 
+// Number 
+// BigInt 
+// null
+// undifined
+// symbol
+// object
+// Booelean
+
+// ------->
+
+
+// console.log(typeof [])
+
+// ----->
+
+// let student1 = "hasnain"
+// let student2 = "shahzaib"   // => many line work
+// let student3 = "random"
+
+// collection of students 
+// group of students
+
+// console.log(student1)
+// console.log(student2)
+// console.log(student3)
+
+// ----->
+
+// let students = ["shahzaib","hasnain","random"]  // convert in one line 
+// let students = [ 0 , 1 , 2]  
+
+// let property = value;
+
+// let alphabets = ["a","b","c"]
+
+
+// console.log(students[0])
+
+// let mobiles = ["vivo","samsung","infinix"]
+
+// console.log(mobiles[1])
+
+
+// Why array is an object in javaScript?
+// JavaScript implements arrays as specialized objects with numbered keys and built-in collection methods.
+// javascript arary ko special object samajta hai 
+// jis may values ko number keys kay zariahai access
+// kar saktay hai or es kay built in collectin methods ki
+//  waja say.
+
+// const colors = ["red","green","blue"]
+// console.log(colors[0])
+
+// Array is global object .
+// used to store an order collection of muiltiple values
+// under a single varibale name. 
+
+// property = (value) <=  single 
+// property = ["value1","value2","Value3"] <=  collection of values
+                //    0  ,    1    ,   2 => order   => tarteb
+                //    2  ,    1    ,   0 => unorder => bagair tarteb
+
+//    ----->
+// Array  => object 
+// collecttion of muiltiple valuse.
+// order collection of muiltipke valuse
+// In a single variable .    
+// How to access it variable ?  
+// colors   [  0  ]  
+// property [index]   
+// Index => position of value in array 
+// --------->
+
+// const number = [1 , 2 ,3]  
+// console.log(number[1])
+
+// ------>
+// Object is standalone , non primitive data struture that 
+// Stores data as a collection of key-value pair.
+
+// ------->
+
+// Object is a standalone,
+// Non-Primitive data structure 
+// That stores data as a collection of key-value pairs.
+
+// let student  = {
+//     name : "abc",
+//     class: 3 ,
+//     city : "karachi" ,
+//     rollNumber : 1234,
+//     //  key  : "value"
+// }
+
+// ---Syntex-->
+
+// let  property = {
+    //   key : value ,
+    //   key : value ,
+    //   key : value ,
+    //   key : value   
+// }
+
+// ----->
+
+// property = (value) => value ki type 
+    // string => "value"
+    // Number => 234
+    // array  => []
+    // object => {}
+    // boolean => true / false
+
+// const person = {
+//     // key : value
+//     name : "shahzaib",
+//     age : 999,
+//     greeting : "hello"
+
+// }
+// --Access object values--->
+// property.key
+// console.log(person.name)
+
+// object / array => 
+// always use const
+
+// const friends = ["name1","name2","name3"]
+// index =>        0  ,    1  ,    2
+// const friends = ["name3","name1","name2"]
+
+
+//------->
+
+// Quick Comparison >
+// Array =>
+// Ordered list / sequence
+// Numeric indices (0, 1, 2...)
+
+
+// object =>
+// Unordered key-value pairs
+// String or Symbol keys
+
+
+
+
+// --Array----->
+// Storing collections of similar items
+// const colors = ["red","blue","green"]
+
+// // Describing a single entity's features
+// const student = {
+//     name : "shahzaib",
+//     age  : 75
+// }
+
+// ------>
+
+
+// Array =>
+// Array is global object.
+// Stored collection of values of similar items.
+// collection of values are ordered
+// How to access its value =>
+// property[index]
+// numbers[1]
+
+// const numbers = [1 ,2 , 3]
+// console.log(numbers[1])
+
+
+// Object =>
+// object stores data as a collection of key-value pair.
+// Where the keys are strings(or symbols)
+// and the values can be any data types 
+// object are unorder
+
+// let person = {
+//       // key    : value
+//     // string : any data type
+//         name  : "string",
+//         age   :  12
+// }
+
+// console.log(person.name)
+
+
+// -------->
+
+// let input = document.getElementById("input")
+// let result = document.getElementById("result")
+
+// function showNote()
+// {
+//    let note = input.value ;
+//    result.innerText += note;
+// }
+
+
+
+
+
+
+
+
