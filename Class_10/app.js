f7283500d70f913b6886e8752731a2ad7e0c4799
shahzaib <kshahzaib07@gmail.com>
2026-07-04 12:57:56 -0700

    added class 10

---
+++ Class_10/app.js
@@ -0,0 +1,150 @@
+// Data types = 8 
+
+// ------>
+
+// primitive = 7
+// non-primitive = 1 
+
+// --------->
+
+// primitive => 
+// store one value        1 = point 
+// Example => a = 10 ;
+
+// simple to define        2 = point
+// Example =>  a = 10 ;
+
+// Immutable (unchanged)   3 = point
+// Example
+// let a = 10 ;
+// let b = a  ;
+// b = 50 ;
+// console.log(a , b)
+
+// copy by value         4 = point 
+// let a = 10 ;
+// let b = a ;
+// copy only value
+
+// compare by value    5 = point 
+// Example =>
+// let a = 10 ;
+// let b = 10 ;
+// console.log(a === b)
+
+// Example => 
+
+
+// --non-Primitive---->
+
+// Store many value.
+// complex to define
+// mutable (can be changed)
+// Copy by reference (address)
+// Compare by reference (address)
+
+// Examples => 
+
+// Store many value.
+// Example => 
+
+// let color = ["c1","c2",c3]
+
+// let obj = { 
+//   name : "ali";
+//   age  :  12 ;
+// }
+
+// complex to define 
+// Example => 
+// let color = ["c1","c2",c3]
+
+// let obj = { 
+//   name : "ali";
+//   age  :  12 ;
+// }
+
+
+
+// mutable (can be changed)
+
+// let obj1 = {
+//     name : "ali"
+// }
+
+// let obj2 = obj1;
+// // console.log(obj2.name)
+// obj2.name = "king"
+// console.log(obj1.name)
+
+// Copy by reference (address)
+
+// let obj1 = {
+//     name : "ali"
+// }
+
+// let obj2 = obj1; 
+
+// Compare by reference (address)
+
+
+// let shahzaib =    // address = 1
+// {
+//     class : "javascript"
+// }
+ 
+// let hussain =   // address  = 2
+// {
+//     class : "javascript"
+// }
+
+// let hamza =   // address  = 3
+// {
+//     class : "javascript"
+// }
+
+
+// console.log(hussain == hamza)
+
+// // -------->
+
+// let hasnain = {    // 4 
+//      class : "javascript"
+// }
+
+// let hammad = hasnain   // 4
+// // hammad => address = 4
+// // hasnain =>  address = 4 
+// console.log(hammad ==  hasnain)
+
+
+
+// Type coersion =>
+
+// changing karna ,  kisa data type 
+
+// coversion type 
+// implicit type conversoin
+// explicit type conversion
+
+// console.log("5" + 2) // => 52
+// // string + number =  string
+
+// String ko jorna
+// number ko plus karana
+
+// console.log("Hello" + "world")
+
+console.log("5" - 2)  // => 3
+//          (5 - 2)
+// sirf number  minus karna
+
+// - only work with numbers , so javascript convert "5" into 5
+
+// type coversion in js 
+
+
+
+
+
+
